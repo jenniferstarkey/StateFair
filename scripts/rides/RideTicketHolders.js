ec1ddfdbb0fa47ed25bae680c6c6c760602d5dfa
Jennifer Starkey <jenniferrstarkey@yahoo.com>
2020-09-03 14:25:41 -0500

    added the ride ticket JS

--- scripts/rides/RideTicketHolders.js
+++ scripts/rides/RideTicketHolders.js
@@ -2,7 +2,11 @@ const contentTarget = document.querySelector(".rides")
 const eventHub = document.querySelector("#state-fair")
 
 export const RideTicketHolders = () => {
-    eventHub.addEventListener("rideTicketPurchased", customEvent => {
-        contentTarget.innerHTML += `<div class="person rider"></div>`
-    })
+    eventHub.addEventListener("ridePurchased", customEvent => {
+        contentTarget.innerHTML += 
+        `<div class="person rider"></div>
+        <div class="person"></div> 
+        <div class="rider></div>`
+    
 }
+    )}
